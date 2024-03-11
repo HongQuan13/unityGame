191dbea4819fff9e4a32ead184b4db1b99495ed4
Quan Nguyen Hong <111102453+HongQuan13@users.noreply.github.com>
2024-03-11 15:08:55 +0800

    resume and stop button

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -11,6 +11,9 @@ public class LogicScript : MonoBehaviour
     public GameObject gameOverScreen;
     public GameObject bird;
     public GameObject gameStart;
+    public GameObject stopScreen;
+    public GameObject gameResume;
+    public Text countdownText;
     PipeSpawnScript pipeSpawnScript;
  
     [ContextMenu("Increase Score")]
@@ -28,7 +31,10 @@ public class LogicScript : MonoBehaviour
     }
     public void startGame()
     {
-        Invoke(nameof(StartSpawning), 5f);
+        gameStart.SetActive(false);
+        stopScreen.SetActive(true); 
+        // Invoke(nameof(StartSpawning), 5f);
+        StartCoroutine(Countdown());
     }
 
     // Method to start the spawning process
@@ -37,7 +43,6 @@ public class LogicScript : MonoBehaviour
         if (pipeSpawnScript != null)
         {
             pipeSpawnScript.StartSpawning();
-            gameStart.SetActive(false);
         }
     }
     public void restartGame()
@@ -47,11 +52,45 @@ public class LogicScript : MonoBehaviour
  
     public void gameOver()
     {
+        stopScreen.SetActive(false);
         gameOverScreen.SetActive(true);
     }
 
-    private void OnDestroy()
+    public void stopGame()
     {
-       
+       if (pipeSpawnScript != null)
+        {
+            pipeSpawnScript.StopSpawning();
+            stopScreen.SetActive(false);
+            gameResume.SetActive(true);
+
+        } 
+
     }
+    public void resumeGame()
+    {
+       if (pipeSpawnScript != null)
+        {
+            StartCoroutine(Countdown());
+            gameResume.SetActive(false);
+            // pipeSpawnScript.StartSpawning();
+            stopScreen.SetActive(true);
+        } 
+
+    }
+    IEnumerator Countdown()
+    {
+        int count = 3;
+        while (count > 0)
+        {
+            // Debug.Log(Time.timeScale);
+            countdownText.text = count.ToString();
+            yield return new WaitForSecondsRealtime(1f); 
+            count--;
+        }
+        countdownText.text = ""; 
+        StartSpawning();
+    }
+
+
 }
