337d3a645616b84290a3c77834d25f1f8e8ae13b
Quan Nguyen Hong <111102453+HongQuan13@users.noreply.github.com>
2024-03-21 19:47:37 +0800

    update profile page

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,20 @@ public class SceneLoader : MonoBehaviour
     public Image burnFatOption;
     public Image fitterOption;
     public Image massOption;
+    public Image monOption;
+    public Image tueOption;
+    public Image wedOption;
+    public Image thuOption;
+    public Image friOption;
+    public Image satOption;
+    public Image sunOption;
+    public GameObject monTable;
+    public GameObject tueTable;
+    public GameObject wedTable;
+    public GameObject thuTable;
+    public GameObject friTable;
+    public GameObject satTable;
+    public GameObject sunTable;
     private float transparentAlpha = 0.1f; // 80% transparent
     private float opaqueAlpha = 0.7f; // 100% opaque
 
@@ -58,6 +72,127 @@ public class SceneLoader : MonoBehaviour
         SetTransparency(burnFatOption, transparentAlpha);
         SetTransparency(fitterOption, transparentAlpha);
         SetTransparency(massOption, opaqueAlpha);
+    }
+
+     public void OnMonClicked()
+    {
+        SetTransparency(monOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(true);
+        tueTable.SetActive(false);
+        wedTable.SetActive(false);
+        thuTable.SetActive(false);
+        friTable.SetActive(false);
+        satTable.SetActive(false);
+        sunTable.SetActive(false);
+        
+    }
+    public void OnTueClicked()
+    {
+        SetTransparency(tueOption, opaqueAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(true);
+        wedTable.SetActive(false);
+        thuTable.SetActive(false);
+        friTable.SetActive(false);
+        satTable.SetActive(false);
+        sunTable.SetActive(false);
+    }
+    public void OnWedClicked()
+    {
+        SetTransparency(wedOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(false);
+        wedTable.SetActive(true);
+        thuTable.SetActive(false);
+        friTable.SetActive(false);
+        satTable.SetActive(false);
+        sunTable.SetActive(false);
+    }
+    public void OnThuClicked()
+    {
+        SetTransparency(thuOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(false);
+        wedTable.SetActive(false);
+        thuTable.SetActive(true);
+        friTable.SetActive(false);
+        satTable.SetActive(false);
+        sunTable.SetActive(false);
+    }
+    public void OnFriClicked()
+    {
+        SetTransparency(friOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(false);
+        wedTable.SetActive(false);
+        thuTable.SetActive(false);
+        friTable.SetActive(true);
+        satTable.SetActive(false);
+        sunTable.SetActive(false);
+    }
+    public void OnSatClicked()
+    {
+        SetTransparency(satOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(sunOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(false);
+        wedTable.SetActive(false);
+        thuTable.SetActive(false);
+        friTable.SetActive(false);
+        satTable.SetActive(true);
+        sunTable.SetActive(false);
+    }
+    public void OnSunClicked()
+    {
+        SetTransparency(sunOption, opaqueAlpha);
+        SetTransparency(tueOption, transparentAlpha);
+        SetTransparency(wedOption, transparentAlpha);
+        SetTransparency(thuOption, transparentAlpha);
+        SetTransparency(friOption, transparentAlpha);
+        SetTransparency(monOption, transparentAlpha);
+        SetTransparency(satOption, transparentAlpha);
+        monTable.SetActive(false);
+        tueTable.SetActive(false);
+        wedTable.SetActive(false);
+        thuTable.SetActive(false);
+        friTable.SetActive(false);
+        satTable.SetActive(false);
+        sunTable.SetActive(true);
     }
 
     // Helper method to set transparency
